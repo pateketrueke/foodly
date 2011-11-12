5a79cf1ee0070326709493d338e07d3c8b99c921
Alvaro Cabrera <pateketrueke@gmail.com>
2011-11-12 15:37:08 -0600

    Por ahora estoy forzando la aplicacion en modo development para debug en Heroku

--- config/application.php
+++ config/application.php
@@ -5,3 +5,5 @@ $config['rewrite'] = 0;
 #$config['temporary_files'] = dirname(__DIR__).DS.'tmp';
 
 $config['environment'] = strpos(value($_SERVER, 'HTTP_HOST'), 'herokuapp') ? 'production' : 'development';
+
+$config['environment'] = 'development';
