83c3dd0d97c508a23a02083c60b2cef2f0c8a25e
Alvaro Cabrera <pateketrueke@gmail.com>
2011-11-13 17:45:03 -0600

    Faltaban keys

--- config/environments/production.php
+++ config/environments/production.php
@@ -1,3 +1,6 @@
 <?php
 $config['rewrite'] = 1;
 $config['dsn'] = str_replace('postgres:', 'pgsql:', getenv('SHARED_DATABASE_URL'));
+
+$config['4sq']['key'] = '5QTCBQV1IBQAPFZ4TYIGFUY130VUN5IW5V2MX4EATYCM3M4X';
+$config['4sq']['secret'] = 'RLWVYWWF4MBLALOX50ZLBC14MOSJPCH5NHGJSK4WN4XGQWFJ';
